b401f05fa6badff760fc783a723ed415589d8eb6
Luana Souza <luana.souza@ifood.com.br>
2021-05-13 17:08:10 -0300

    App tsk com SharedPreferences

---
+++ app/src/main/java/br/com/luanadev/tasks/data/Task.kt
@@ -0,0 +1,14 @@
+package br.com.luanadev.tasks.data
+
+import java.util.*
+
+enum class TaskPriority {
+    HIGH, MEDIUM, LOW
+}
+
+data class Task(
+    val name: String,
+    val deadline: Date,
+    val priority: TaskPriority,
+    val completed: Boolean = false
+)
